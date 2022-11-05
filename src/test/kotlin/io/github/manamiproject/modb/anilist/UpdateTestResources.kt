87176d5baa20adc23b71f205d540ff851803ceb0
manami-project <13179375+manami-project@users.noreply.github.com>
2022-11-05 11:28:39 +0100

    Fixed scope of main

--- src/test/kotlin/io/github/manamiproject/modb/anilist/UpdateTestResources.kt
+++ src/test/kotlin/io/github/manamiproject/modb/anilist/UpdateTestResources.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 import java.nio.file.Path
 import java.nio.file.Paths
 
-fun main() {
+internal fun main() {
     val downloader = AnilistDownloader(AnilistConfig)
     
     runBlocking { 
